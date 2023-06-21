e4a7c31906c875b654ac8ef8c58cf370204a76e9
Константин Ларионов <kostya12277@yandex.ru>
2023-06-21 09:54:47 +0500

    First lesson

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,8 +7,14 @@ namespace TestConsole
 {
     class Program
     {
-        static void Main(string[] args )
+        static void Main(string[] args) 
         {
+            int balance = (1000);
+            int price = Convert.ToInt32 (Console.ReadLine());
+            int weight = Convert.ToInt32 (Console.ReadLine());
+            int c = (price * weight);
+            balance += c;
+            Console.WriteLine (balance);
         }
     }
  
